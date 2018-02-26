6f3d90468e759a9522630bc4520d670a610f08b4
mawenjie <mawenjie@dwnews.com>
2018-02-26 11:17:34 +0800

    修改模板

--- SpringBootDemo/src/main/java/com/example/demo/controller/testController.java
+++ SpringBootDemo/src/main/java/com/example/demo/controller/testController.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
 public class testController {
 	
-	@RequestMapping("/hello")
+	@RequestMapping("/")
 	public String hello() {
 		return "Hello World!";
 	}
